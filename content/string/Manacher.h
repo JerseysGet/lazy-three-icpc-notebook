b439a94a574d37f6198fadef0a51cc081543b76d
JerseysGet <dev.faisal2004@gmail.com>
2024-03-17 12:35:34 +0530

    added better description for Manachers

--- content/string/Manacher.h
+++ content/string/Manacher.h
@@ -1,15 +1,15 @@
-/* Description: p[0][i] = half length of longest even palindrome around pos i, p[1][i] = longest odd (half rounded down). */
-
+/* Description: p[0][i] = half length of longest even palindrome behind pos i, 
+p[1][i] = longest odd with center at pos i(half rounded down). */
 array<vi, 2> manacher(const string& s) {
-	int n = sz(s);
-	array<vi,2> p = {vi(n+1), vi(n)};
-	rep(z,0,2) for (int i=0,l=0,r=0; i < n; i++) {
-		int t = r-i+!z;
-		if (i<r) p[z][i] = min(t, p[z][l+t]);
-		int L = i-p[z][i], R = i+p[z][i]-!z;
-		while (L>=1 && R+1<n && s[L-1] == s[R+1])
-			p[z][i]++, L--, R++;
-		if (R>r) l=L, r=R;
-	}
-	return p;
+    int n = sz(s);
+    array<vi, 2> p = {vi(n + 1), vi(n)};
+    for (int z = 0; z < 2; z++) for (int i = 0, l = 0, r = 0; i < n; i++) {
+        int t = r - i + !z;
+        if (i < r) p[z][i] = min(t, p[z][l + t]);
+        int L = i - p[z][i], R = i + p[z][i] - !z;
+        while (L >= 1 && R + 1 < n && s[L - 1] == s[R + 1])
+            p[z][i]++, L--, R++;
+        if (R > r) l = L, r = R;
+    }
+    return p;
 }
